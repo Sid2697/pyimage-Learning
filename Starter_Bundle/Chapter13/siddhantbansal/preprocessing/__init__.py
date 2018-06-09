8cb18d383ac580fd1282c83744b80f198ecf7b93
Siddhant Bansal <siddhant2697@gmail.com>
2018-06-09 06:31:46 +0530

    Add files via upload

---
+++ Starter_Bundle/Chapter13/siddhantbansal/preprocessing/__init__.py
@@ -0,0 +1,4 @@
+# importing necessary packages
+from .imagetoarraypreprocessor import ImageToArrayPreprocessor
+from .simplepreprocessor import SimplePreprocessor
+from .simpledatasetloader import SimpleDatasetLoader
